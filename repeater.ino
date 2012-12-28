a92f69b1a2e3eb3f003efeb31a6d28a148f996c4
Sebastien Van Cauwenberghe <svancau@gmail.com>
2012-12-28 15:31:47 +0100

    Added preliminary DDS based tone generation (with fixed pins now)

--- repeater.ino
+++ repeater.ino
@@ -30,7 +30,8 @@
 #define PIN_CTCSS 5
 #define PIN_CARRIER 6
 #define PIN_PTT 13
-#define PIN_MORSEOUT 14
+// PIN for MORSE OUTPUT is fixed to 6 for Arduino Leonardo
+// /!\ WARNING DO NOT USE tone() function as we act directly on TIMERS
 
 // DELAYS (in ms)
 #define INACTIVE_CLOSE 10000
@@ -133,12 +134,31 @@ const unsigned char morse_ascii[] = {
   MORSE_NONE, MORSE_NONE, MORSE_NONE, MORSE_NONE,
 };
 
+// Sine wave table
+PROGMEM prog_uchar sine[] = {
+  127,130,133,136,139,143,146,149,152,155,158,161,164,167,170,173,
+  176,178,181,184,187,190,192,195,198,200,203,205,208,210,212,215,
+  217,219,221,223,225,227,229,231,233,234,236,238,239,240,242,243,
+  244,245,247,248,249,249,250,251,252,252,253,253,253,254,254,254,
+  254,254,254,254,253,253,253,252,252,251,250,249,249,248,247,245,
+  244,243,242,240,239,238,236,234,233,231,229,227,225,223,221,219,
+  217,215,212,210,208,205,203,200,198,195,192,190,187,184,181,178,
+  176,173,170,167,164,161,158,155,152,149,146,143,139,136,133,130,
+  127,124,121,118,115,111,108,105,102,99,96,93,90,87,84,81,78,76,
+  73,70,67,64,62,59,56,54,51,49,46,44,42,39,37,35,33,31,29,27,25,
+  23,21,20,18,16,15,14,12,11,10,9,7,6,5,5,4,3,2,2,1,1,1,0,0,0,0,
+  0,0,0,1,1,1,2,2,3,4,5,5,6,7,9,10,11,12,14,15,16,18,20,21,23,25,
+  27,29,31,33,35,37,39,42,44,46,49,51,54,56,59,62,64,67,70,73,76,
+  78,81,84,87,90,93,96,99,102,105,108,111,115,118,121,124
+};
+
 /////////////////////////////////////////////
 // Setup phase
 /////////////////////////////////////////////
 void setup()
 {
-  ioSetup();  
+  ioSetup();
+  setupTimer();
   Serial.begin(115200);
 #if (!USE_DEBUGMODE)
   wdt_enable (WDTO_8S); // Enable watchdog
@@ -175,6 +195,8 @@ ulong timeoutTimer; // Cut too long keydowns
 ulong pttEnableTimer; // PTT enabled to morse
 ulong pttDisableTimer; // End of morse to PTT off
 ulong carrier1750openTimer; // Carrier/1750 length before opening
+
+ulong ddsPhaseAccu; // DDS Phase accumulator
 
 bool beepEnabled; // A Roger beep can be sent
 bool sqlOpen; // Current Squelch status
@@ -344,7 +366,7 @@ void rogerBeep()
 {
 #ifdef ROGER_TONE
   if (!morseActive)
-  startBeep (PIN_MORSEOUT, BEEP_FREQ, BEEP_LENGTH);
+    startBeep (BEEP_FREQ, BEEP_LENGTH);
 #endif
 
 #ifdef ROGER_K
@@ -383,15 +405,29 @@ void updateIO()
   else
     digitalWrite(PIN_PTT, LOW);
 
-  updateBeep(PIN_MORSEOUT); // Stop the beep when timer elapsed
+  updateBeep(); // Stop the beep when timer elapsed
+}
+
+// Configure timer for morse PWM
+void setupTimer()
+{
+#if defined(__AVR_ATmega32U4__)
+  TCCR3A = (1 << WGM30); // Enable Fast PWM mode
+  TCCR3B = (1 << WGM32) | (1 << CS30); // Prescaler = 1
+  DDRC |= (1 << DDC6); // Pin 6 as output
+#endif
 }
 
 // Do a beep for a known time
-void startBeep(unsigned int pin, unsigned int freq, unsigned long duration)
+void startBeep(unsigned int freq, unsigned long duration)
 {
   if (!beepOn)
   {
-    tone (pin, freq);
+#if defined(__AVR_ATmega32U4__)
+    TCCR3A |= (1 << COM3A1); // Enable comparator output
+    TIMSK3 |= (1 << TOIE3); // Enable Timer interrupt
+#endif
+
     UPDATE_TIMER(beepToneTimer,duration);
     beepOn = true;
   }
@@ -399,11 +435,15 @@ void startBeep(unsigned int pin, unsigned int freq, unsigned long duration)
 
 // Check that the beep time is elapsed and stop it if needed
 // CALLED only once in updateIO
-void updateBeep(unsigned int pin)
+void updateBeep()
 {
   if (beepOn && TIMER_ELAPSED(beepToneTimer))
   {
-    noTone(pin);
+#if defined(__AVR_ATmega32U4__)
+    TCCR3A &= ~(1 << COM3A1); // Enable comparator output
+    TIMSK3 &= ~(1 << TOIE3); // Enable Timer interrupt
+#endif
+
     beepOn = false;
   }
 }
@@ -502,7 +542,7 @@ void morseDash()
 {
   if (TIMER_ELAPSED(morseTimer))
   {
-    startBeep (PIN_MORSEOUT, MORSE_FREQ, MORSE_DASH);
+    startBeep (MORSE_FREQ, MORSE_DASH);
     UPDATE_TIMER(morseTimer,MORSE_DASH+MORSE_SYM_SPC);
   }
 }
@@ -511,8 +551,19 @@ void morseDot()
 {
   if (TIMER_ELAPSED(morseTimer))
   {
-    startBeep (PIN_MORSEOUT, MORSE_FREQ, MORSE_DOT);
+    startBeep (MORSE_FREQ, MORSE_DOT);
     UPDATE_TIMER(morseTimer,MORSE_DOT+MORSE_SYM_SPC);
   }
 }
 
+// Tone generation DDS timer overflow interrupt
+// For Arduino Leonardo
+#if defined(__AVR_ATmega32U4__)
+ISR(TIMER3_OVF_vect)
+{
+  unsigned char index; // Sample Index
+  ddsPhaseAccu += 0x04400000UL;
+  index = ddsPhaseAccu >> 24;
+  OCR3A = pgm_read_byte_near(sine + index);
+}
+#endif
